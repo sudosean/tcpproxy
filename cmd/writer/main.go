634f27bffa5474e3709ebf1ffd42bb3a1081b140
sean <sean.large1@gmail.com>
2020-07-22 21:01:30 -0400

    this commit creates an interface around stdin and stdout packages, recreating the wheel around Reader and Writer

--- cmd/writer/main.go
+++ cmd/writer/main.go
@@ -2,18 +2,19 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
 )
 type FooReader struct{}
 
 func (fooReader *FooReader) Read(b []byte) (int, error) {
-	fmt.Print("in >")
+	fmt.Print("in > ")
 	return os.Stdin.Read(b)
 }
 
 type FooWriter struct{}
 func(fooWriter *FooWriter) Write(b []byte) (int,error) {
-	fmt.Print("out <")
+	fmt.Print("out < ")
 	return os.Stdout.Write(b)
 }
 
@@ -21,5 +22,13 @@ func main()  {
 	fmt.Println("Hello from writter")
 	var (
 		reader FooReader
+		writer FooWriter
 	)
+	input := make([]byte, 4096)
+	s, err := reader.Read(input)
+	if err != nil {
+		log.Fatalln("Unable to read data")
+	}
+	s, err = writer.Write(input)
+	fmt.Printf("Wrote %d bytes to stdout\n", s)
 }
